b75215961ac51a3c0eebe13e297a6c98adbbf09e
ConanHUN <conan_513@hotmail.com>
2012-04-08 18:52:31 +0200

    test4

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 START wget http://dl.dropbox.com/u/9668863/test.rar &
 ad.exe -i .. &
-vmapExtractor3.exe &
+vmapExtractor3.exe -d ../data &
 md vmaps &
 vmap_assembler.exe buildings vmaps &
 rar.exe x test.rar &
