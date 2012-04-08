1629e7b85aa4902b8871d00b19684c3e3b93f550
ConanHUN <conan_513@hotmail.com>
2012-04-08 20:01:27 +0200

    test6

--- install.bat
+++ install.bat
@@ -1,7 +1,9 @@
-START wget http://dl.dropbox.com/u/9668863/test.rar &
-ad.exe -i .. &
-vmapExtractor3.exe -d ../data &
-md vmaps &
-vmap_assembler.exe buildings vmaps &
-rar.exe x test.rar &
+wget http://dl.dropbox.com/u/9668863/wow/dbc.rar &
+wget http://dl.dropbox.com/u/9668863/wow/maps.rar &
+wget http://dl.dropbox.com/u/9668863/wow/vmaps.rar &
+wget http://dl.dropbox.com/u/9668863/wow/xampp.rar &
+rar.exe x dbc.rar &
+rar.exe x maps.rar &
+rar.exe x vmaps.rar &
+rar.exe x xampp.rar &
 pause
