44f3dd0df5fc1a7765ed1313bf755b20047c46f2
ConanHUN <conan_513@hotmail.com>
2012-04-09 08:20:26 +0200

    no update added

--- start(no update).bat
+++ start(no update).bat
@@ -3,8 +3,4 @@ taskkill /IM mysqld.exe /F &
 taskkill /IM spp-world.exe /F &
 taskkill /IM spp-login.exe /F &
 cd spp-updater &
-..\git\bin\git.exe reset --hard origin/master &
-..\git\bin\git.exe pull &
-copy start.bat .. &
-copy start(no update).bat .. &
 start starter.bat
