75db78f6abb96d7c89e91fa0d6e1a45fe813a052
ConanHUN <conan_513@hotmail.com>
2012-04-09 07:09:00 +0200

    another test

--- start.bat
+++ start.bat
@@ -1,5 +1,9 @@
+taskkill /IM mysql.exe /F &
+taskkill /IM mysqld.exe /F &
+taskkill /IM spp-world.exe /F &
+taskkill /IM spp-login.exe /F &
 cd spp-updater &
+..\git\bin\git.exe reset --hard origin/master &
 ..\git\bin\git.exe pull &
 copy start.bat .. &
-cd .. &
 start starter.bat
--- starter.bat
+++ starter.bat
@@ -1,7 +1,3 @@
-taskkill /IM mysql.exe /F &
-taskkill /IM mysqld.exe /F &
-taskkill /IM spp-world.exe /F &
-taskkill /IM spp-login.exe /F &
 cd server\xampp\ &
 start mysql_start.bat &
 cd ../.. &
