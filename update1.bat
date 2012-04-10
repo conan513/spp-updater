b3fd5fa3ac52d30e27c0ac1304621d397fcdcdaa
ConanHUN <conan_513@hotmail.com>
2012-04-10 12:06:48 +0200

    mmaps fix

--- update1.bat
+++ update1.bat
@@ -4,7 +4,7 @@ echo MMaps most telepulni fog a szerverre. Ez tobb percet is igenybe vehet!
 sleep 10
 wget -c http://dl.dropbox.com/u/9668863/wow/mmaps.rar &
 rar.exe x mmaps.rar &
-del maps.rar &
+del mmaps.rar &
 copy updates\after\start_up1.bat ..\start.bat &
 cd .. &
 start start.bat &
