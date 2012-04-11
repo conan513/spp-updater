570415c04435ba880cc842526f7fc2ad4a93e4a5
ConanHUN <conan_513@hotmail.com>
2012-04-11 23:18:10 +0200

    IP-config fix

--- updates/u2-lan/import.bat
+++ updates/u2-lan/import.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Most elindul Notepad++ ahol latni fogsz egy sort.
-echo Keresd meg benne az IP-t (alapesetben 127.0.0.1) es ird at a LAN vagy INTERNET
-echo IP-re, ha a szerver masik gepen fut mint a WoW.
+echo Keresd meg benne az IP-t (alapesetben 127.0.0.1) es ird at
+echo a LAN vagy INTERNET IP-re, esetleg vissza 127.0.0.1-re. (Offline mod) 
 echo ..................................................
 echo Ha atirtad az IP-t nyomj meg egy gombot.
 echo Ha meggondoltad magad X-el zard be ezt az ablakot.
@@ -14,9 +14,10 @@ taskkill /IM spp-login.exe /F &
 cd ..\..\server\xampp\ &
 start mysql_start.bat &
 cd ..\.. &
+cls
 echo Egy kis turelem...
 sleep 10 &
-cd ..\..\updates\u2-lan &
+cd updates\u2-lan &
 unicode\Notepad++.exe realmlist.sql
 ..\..\mysql -u root -p123456 realmd < realmlist.sql
 taskkill /IM mysqld.exe /F &
