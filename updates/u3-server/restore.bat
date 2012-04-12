07eab0bd8784286a0589930516a01459c5af0fe0
ConanHUN <conan_513@hotmail.com>
2012-04-12 11:05:24 +0200

    restore

--- updates/u3-server/restore.bat
+++ updates/u3-server/restore.bat
@@ -1,4 +1,9 @@
 @echo off
+echo Figyelem!!
+echo Ezt a fajlt csak abban az esetben hasznald, 
+echo ha egy update utan nem mukodne a szerver!
+echo Ha a szerver megfeleloen mukodik, akkor zard be ezt az ablakot most.
+pause
 taskkill /IM mysql.exe /F &
 taskkill /IM mysqld.exe /F &
 taskkill /IM spp-world.exe /F &
