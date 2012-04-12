1738c3fb7c47bacb19433f5111ce916a59486985
ConanHUN <conan_513@hotmail.com>
2012-04-12 11:12:55 +0200

    restore

--- updates/u3-server/restore.bat
+++ updates/u3-server/restore.bat
@@ -9,7 +9,7 @@ taskkill /IM mysqld.exe /F &
 taskkill /IM spp-world.exe /F &
 taskkill /IM spp-login.exe /F &
 cd spp-updater &
-..\git\bin\git.exe reset --hard
+..\git\bin\git.exe reset --hard &
 ..\git\bin\git.exe pull &
 copy updates\u3-server\start.bat .. &
 copy IP-config.bat .. &
