5afcd50ce59d2c3c3274ff965c763f46488ec4ed
ConanHUN <conan_513@hotmail.com>
2012-04-12 09:57:12 +0200

    update 3

--- updates/u3-server/update.bat
+++ updates/u3-server/update.bat
@@ -5,9 +5,7 @@ taskkill /IM spp-world.exe /F &
 taskkill /IM spp-login.exe /F &
 cd spp-updater &
 ..\git\bin\git.exe pull &
-copy updates\after\start_up1.bat ..\start.bat &
+copy updates\u3-server\start.bat .. &
 copy IP-config.bat .. &
 copy "start(no update).bat" .. &
-copy updates\after\update.bat ..\update.bat
-cd updates\u3-server
-start update3.bat
+copy updates\u3-server\update.bat .. &
---
+++ updates/u3-server/update3_start.bat
@@ -0,0 +1,28 @@
+@echo off
+echo Update 3: Szerver frissites
+echo Szerver es az adatbazis frissulni fog. Ez tobb percet is igenybe vehet!
+..\..\sleep 10
+
+taskkill /IM mysql.exe /F &
+taskkill /IM mysqld.exe /F &
+taskkill /IM spp-world.exe /F &
+taskkill /IM spp-login.exe /F &
+
+..\..\wget -c http://dl.dropbox.com/u/9668863/wow/up3/mangos.rar &
+..\..\wget -c http://dl.dropbox.com/u/9668863/wow/up3/scriptdev2.sql &
+
+..\..\rar.exe x mangos.rar &
+del mangos.rar &
+
+..\..\mysql -u root -p123456 < delete.sql
+..\..\mysql -u root -p123456 < create.sql
+..\..\mysql -u root -p123456 mangos < mangos.sql
+..\..\mysql -u root -p123456 scriptdev2 < scriptdev2.sql
+
+del mangos.sql
+del scriptdev2.sql
+
+copy start.bat ..\..\..\start.bat &
+cd ..\..\..\ &
+start start.bat &
+exit
